9b4f5239c593a4b115f79cb46bbbf14924f7bb08
Mrafuel <raphaelosundwa@gmail.com>
2023-09-07 13:35:42 -0700

    Correction_4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,7 +6,7 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	("\"Programming is like building a multilingual puzzle\n");
 
-    return (0);
+	return (0);
 }
