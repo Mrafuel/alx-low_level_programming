de079f76c30fef3990a6bfe1fe55a90fbea29233
Mrafuel <raphaelosundwa@gmail.com>
2023-09-11 07:55:33 -0700

    Corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /*
  * Main- Entry point.
  *
- * Description: Prints various types on the computer it is compiled and run on.
+ * Description:Write a C program that prints the size of various types on the computer it is compiled and run on.
  *
  *Return: Always 0.
  */
