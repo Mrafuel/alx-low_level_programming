33ef5c5c7e8a6f804b9a59f5bb981d2c9510886c
Mrafuel <raphaelosundwa@gmail.com>
2023-09-11 05:32:50 -0700

    Corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
-/*
- *Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
- *
- * 
- */
-
+#include<stdio.h
 #include <unistd.h>
-
+/*
+ *
+ * " and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ *
+ */
 int main(void)
 {
-	write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n" 56);
 
 	return(1);
 }
