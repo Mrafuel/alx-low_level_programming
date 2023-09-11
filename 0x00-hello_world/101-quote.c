81624fe9e1703d0bff8b82659d85e9b7b0274064
Mrafuel <raphaelosundwa@gmail.com>
2023-09-11 08:58:58 -0700

    Corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <unistd.h>
 /*
- * main - Prints out and that piece of art is useful" - Dora Korpar, 2015-10-19 to standard error.
- *
- *
+ * main - Print " and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * to standard error.
  * Return: Always 1.
  */
 	int main(void)
 {
-		fprintf(stdeer, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
-        return(1);
+		write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return(1);
 }
