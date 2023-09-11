d2d01c129cdffad988594350f69129b58dfd9073
Mrafuel <raphaelosundwa@gmail.com>
2023-09-11 07:03:01 -0700

    main - Prints the size of various types based on  *        the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /*
- *
- *
- * main - Write a C program that prints the size of various types on the computer it is compiled and run on.
+ *main - Entry point 
+ *Description:- Write a C program that prints the size of various types on the computer it is compiled and run on.
+ *Return: Always 0 -Success
  *
  */
 int main(void)
