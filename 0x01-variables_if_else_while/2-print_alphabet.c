602cda59a64a6768476c6656b4b91ebcc3f27f0b
Mrafuel <raphaelosundwa@gmail.com>
2023-09-11 14:53:14 -0700

    Correction

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - This is the Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 
 int main(void)
 {
-	char low;
+	char lowCase;
 
-	for (low = 'a'; low <= 'z'; low++)
+	for (lowCase = 'a'; lowCase <= 'z'; lowCase++)
 	{
-		putchar(low);
-		putchar('\n');
+		putchar(lowCase);
 	}
 	return (0);
 }
