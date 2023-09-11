b1c207db9b0855509cc292e56887cec64dcbd561
Mrafuel <raphaelosundwa@gmail.com>
2023-09-11 09:22:27 -0700

    Corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,12 +2,12 @@
 /*
  * main- Entry point.
  *
- * description-  Prints the size of various types on the computer and run on
  *
  * Return: Always 0 (Success)
  *
  */
-	int main(void){
+	int main(void)
+	{
 
 		printf("Size of a char: %zu byte(s)\n", sizeof(char));
 		printf("Size of an int: %zu byte(s)\n", sizeof(int));
