27ed3108db13c341bfebf6b5ed614c875434d8a1
Mrafuel <raphaelosundwa@gmail.com>
2023-09-11 02:14:02 -0700

    return 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+/*
+ *Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ *
+ * 
+ */
+
+#include<stdi0.h>
+
+int main(void)
+{
+	write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+
+	return(1);
+}
