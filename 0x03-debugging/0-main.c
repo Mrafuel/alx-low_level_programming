2d83b7da8c2473d7de4aa12d0803aa8bf714d9f7
Mrafuel <raphaelosundwa@gmail.com>
2023-09-14 13:08:19 -0700

    corrctions

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,12 +4,9 @@
  * main -Tests functions that prints if an integer is +ve or -ve
  * Return: 0
  */
-
-	int main(void)
-	{
-		int i;
-		int i = 0;
-		positive_or_negative(1);
-		
-		return (0);
-	}
+int main(void)
+{
+	int i =0;
+	positive_or_negative(i);
+	return (0);
+}
